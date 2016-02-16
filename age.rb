13cc37bcab53943f5abd3ea14f32b0d40ef38198
darkmangh <charles_rex@yahoo.com>
2016-02-16 17:59:50 +0000

    age

---
+++ age.rb
@@ -0,0 +1,20 @@
+puts "How old are you?"
+
+age = gets.chomp.to_i
+
+puts "Your age in 10 yrs is:"
+
+puts 10 + age
+
+puts "Your age in 20 yrs is:"
+
+puts 20 + age
+
+puts "Your age in 30 yrs is:"
+
+puts 30 + age
+
+puts "Your age in 40 yrs is:"
+
+puts 30 + age
+
